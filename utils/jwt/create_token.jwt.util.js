44897ed4ff657ff28c3a0ded30bfa27352c5c0d0
AdityaKumar5155 <adityakumar5155@gmail.com>
2026-06-13 15:21:59 +0530

    chore: added name in jwt token

--- utils/jwt/create_token.jwt.util.js
+++ utils/jwt/create_token.jwt.util.js
@@ -2,7 +2,13 @@ const jwt = require('jsonwebtoken');
 
 const createToken = (user, expiresIn = '12h') => {
     const token = jwt.sign(
-        { id: user.id, email: user.email },
+        { 
+            id: user.id, 
+            first_name: user.first_name, 
+            middle_name: user.middle_name,
+            last_name: user.last_name, 
+            email: user.email 
+        },
         process.env.JWT_SECRET,
         { expiresIn }
     );
